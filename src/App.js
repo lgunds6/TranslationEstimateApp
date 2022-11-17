e282c2a7880ca1b4c9f307fecf5edc046fe47b71
LukeGunders <lgunds6@gmail.com>
2022-11-17 12:20:50 +1100

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ import React, {
 import Checkers from "./Checkers.js";
 import TranslationQuote from "./components/TranslationQuote.tsx";
 import CustomSelect from "./components/CustomSelect.tsx";
-import PDFView from "./components/PDFView.js";
-import { PDFViewer } from "@react-pdf/renderer";
-import { UseProvider } from "./components/UserContext.js";
 
 function App(props) {
   const [ticked, setTicked] = useState(false);
